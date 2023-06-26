342dafa4e7cbb28292562258387e020f5f684ca8
Santi Legaristi Royo <slegaris@c1r13s6.42madrid.com>
2023-06-26 21:42:37 +0200

    it works with flags and absolute paths as well as command names only

--- ft_env.c
+++ ft_env.c
@@ -6,7 +6,7 @@
 /*   By: slegaris <slegaris@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:54:17 by slegaris          #+#    #+#             */
-/*   Updated: 2023/06/26 20:16:59 by slegaris         ###   ########.fr       */
+/*   Updated: 2023/06/26 21:39:57 by slegaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,33 +33,94 @@ char	*ft_check_path(char *path_var, char *cmd)
 	}
 	return (NULL);
 }
-
-char	*ft_get_command_path(char **envp, char *cmd)
+//
+// char	*ft_get_command_path(char **envp, char *cmd)
+// {
+// 	char	*path_var;
+// 	char	*result;
+// 	char	**cmd_parts;
+//
+// 	cmd_parts = ft_split(cmd, ' ');
+//
+// 	path_var = NULL;
+// 	while (*envp)
+// 	{
+// 		if (ft_strncmp(*envp, "PATH=", 5) == 0)
+// 		{
+// 			path_var = *envp + 5;
+// 			break;
+// 		}
+// 		envp++;
+// 	}
+// 	result = ft_check_path(path_var, cmd_parts[0]);
+// 	if (result)
+// 	{
+// 		free(cmd_parts[0]);
+// 		cmd_parts[0] = result;
+// 		return (ft_strjoin(cmd_parts[0], " "));
+// 	}
+// 	if (access(cmd_parts[0], X_OK) == 0)
+// 		return (cmd);
+// 	return (free(cmd_parts), NULL);
+// }
+char *ft_get_path_var(char **envp)
 {
-	char	*path_var;
-	char	*result;
-	char	**cmd_parts;
+    while (*envp)
+    {
+        if (ft_strncmp(*envp, "PATH=", 5) == 0)
+            return (*envp + 5);
+        envp++;
+    }
+    return (NULL);
+}
 
-	cmd_parts = ft_split(cmd, ' ');
+char *ft_check_command_path(char *cmd, char **cmd_parts, char **envp)
+{
+    char    *result;
 
-	path_var = NULL;
-	while (*envp)
-	{
-		if (ft_strncmp(*envp, "PATH=", 5) == 0)
-		{
-			path_var = *envp + 5;
-			break;
-		}
-		envp++;
-	}
-	result = ft_check_path(path_var, cmd_parts[0]);
-	if (result)
-	{
-		free(cmd_parts[0]);
-		cmd_parts[0] = result;
-		return (ft_strjoin(cmd_parts[0], " "));
-	}
-	if (access(cmd_parts[0], X_OK) == 0)
-		return (cmd);
-	return (free(cmd_parts), NULL);
+    result = ft_check_path(ft_get_path_var(envp), cmd_parts[0]);
+    if (result)
+    {
+        free(cmd_parts[0]);
+        cmd_parts[0] = result;
+    }
+    else if (access(cmd_parts[0], X_OK) == 0)
+        result = cmd;
+    else
+        result = NULL;
+    return (result);
+}
+
+char *ft_join_cmd_and_args(char **cmd_parts)
+{
+    int     i;
+    char    *command_with_args;
+    char    *tmp;
+
+    i = 0;
+    command_with_args = ft_strdup(cmd_parts[i]);
+    i++;
+    while (cmd_parts[i])
+    {
+        tmp = ft_strjoin(command_with_args, " ");
+        free(command_with_args);
+        command_with_args = ft_strjoin(tmp, cmd_parts[i]);
+        free(tmp);
+        i++;
+    }
+    return (command_with_args);
+}
+
+char *ft_get_command_path(char **envp, char *cmd)
+{
+    char    *result;
+    char    **cmd_parts;
+
+    cmd_parts = ft_split(cmd, ' ');
+
+    result = ft_check_command_path(cmd, cmd_parts, envp);
+    if (result)
+        result = ft_join_cmd_and_args(cmd_parts);
+    free(cmd_parts);
+    return (result);
 }
